57f462fb20b4bfd27465d6fbe4bbfad80b6697c8
Binary Wang <binarywang@gmail.com>
2016-02-29 22:15:28 +0800

    update testcase

--- src/test/java/cn/edu/buaa/binarywang/hadoop/practice/LogParserTest.java
+++ src/test/java/cn/edu/buaa/binarywang/hadoop/practice/LogParserTest.java
@@ -9,32 +9,40 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
+import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+
 import cn.edu.buaa.practice.bean.LogRecord;
 import cn.edu.buaa.practice.parser.LogParser;
 
-import com.google.common.base.Optional;
-import com.google.common.io.Files;
-
 public class LogParserTest {
 
-	@Test
-	public void testParse() throws IOException {
-		List<String> readLines = Files.readLines(new File("src/test/resources/access.log.10"),
-				Charset.forName("utf-8"));
-		int count = 0;
-		for (String line : readLines) {
-			try {
-				Optional<LogRecord> logRecord = LogParser.parse(line);
-				if (logRecord.isPresent()) {
-					System.err.println(ToStringBuilder.reflectionToString(logRecord.get(), ToStringStyle.MULTI_LINE_STYLE));
-					System.err.println(logRecord.get().getAccessTime());
-				} else {
-					count++;
-				}
-			} catch (Exception e) {
-				System.err.println(line);
-			}
-		}
-		System.out.println(count);
-	}
+    @Test
+    public void testParse() throws IOException {
+        List<String> readLines = Files.readLines(new File("src/test/resources/access.log.10"),
+                Charset.forName("utf-8"));
+        List<String> illegalLogRecords = Lists.newArrayList();
+        for (String line : readLines) {
+            try {
+                Optional<LogRecord> logRecord = LogParser.parse(line);
+                if (logRecord.isPresent()) {
+                    System.err.println(
+                            ToStringBuilder.reflectionToString(logRecord.get(), ToStringStyle.MULTI_LINE_STYLE));
+                    System.err.println(logRecord.get().getAccessTime());
+                } else {
+                    illegalLogRecords.add(line);
+                }
+            } catch (Exception e) {
+                illegalLogRecords.add(line);
+            }
+
+        }
+
+        System.err.println("==============illegal logs================");
+        System.err.println("无用日志条数：" + illegalLogRecords.size());
+        System.err.println(Joiner.on("\n").join(illegalLogRecords));
+    }
+
 }
